0e3bb60c6b05fe25cbd016b1f026d75386972937
jrs2ea <jrs2ea@virginia.edu>
2011-10-22 02:24:10 -0300

    Comments, formatting

--- backbone.slet.js
+++ backbone.slet.js
@@ -1,3 +1,4 @@
+//Scope issue fix
 // Wrap an optional error callback with a fallback error event.
 var wrapError = function(onError, model, options) {
   return function(resp) {
@@ -9,49 +10,50 @@ var wrapError = function(onError, model, options) {
   };
 };
 
+// Simply goes through the local property
 var getLocal = function(query) {
 	return this.get('local')[query];
 };
 
+// Same but checks for existence
 var setLocal = function(localObject) {
 	var local = this.get('local');
 	if(typeof(local)==='undefined') {
 		local = {};
 	}
+	//'local' inherits localObject properties
 	_(local).extend(localObject);
 	this.set({'local':local});
 };
 
+// extend Model
 var Slet = Backbone.Model.extend({
-	
-	
-	getLocal: getLocal,
-	
-	glet : getLocal,
-	
-	setLocal: setLocal,
-	
-	slet : setLocal,
+  // attach slet methods
+  getLocal: getLocal,
+  glet : getLocal,
+  setLocal: setLocal,
+  slet : setLocal,
 
-    // From Backbone.js 0.5.3
-    save : function(attrs, options) {
-      options || (options = {});
-      if (attrs && !this.set(attrs, options)) return false;
-      var model = this;
-      //INSERTION
-      var noLocalThis = this.clone();
-      if(typeof(noLocalThis.get('local'))!=='undefined') {
-        noLocalThis.unset('local');
-      }
-      //END INSERTION
-      var success = options.success;
-      options.success = function(resp, status, xhr) {
-        if (!model.set(model.parse(resp, xhr), options)) return false;
-        if (success) success(model, resp, xhr);
-      };
-      options.error = wrapError(options.error, model, options);
-      var method = this.isNew() ? 'create' : 'update';
-      return (this.sync || Backbone.sync).call(this, method, noLocalThis, options);
+  // From Backbone.js 0.5.3
+  save : function(attrs, options) {
+    options || (options = {});
+    if (attrs && !this.set(attrs, options)) return false;
+    var model = this;
+    //INSERTION
+    var noLocalThis = this.clone();
+    if(typeof(noLocalThis.get('local'))!=='undefined') {
+      noLocalThis.unset('local');
     }
-	
+    //END INSERTION
+    var success = options.success;
+    options.success = function(resp, status, xhr) {
+      if (!model.set(model.parse(resp, xhr), options)) return false;
+      if (success) success(model, resp, xhr);
+    };
+    options.error = wrapError(options.error, model, options);
+    var method = this.isNew() ? 'create' : 'update';
+    //INSERTION
+    return (this.sync || Backbone.sync).call(this, method, noLocalThis, options);
+    //END INSERTION
+  }	
 });
